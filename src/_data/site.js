155617447637d71afe6d390dfb5bb361b0f61fdc
R00taryEnginE <13239555+R00taryEnginE@users.noreply.github.com>
2025-12-21 07:03:43 +0200

    Update base url
    Signed-off-by: R00taryEnginE <13239555+R00taryEnginE@users.noreply.github.com>

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,6 +1,6 @@
 module.exports = {
 	"title": "AA2 Cardmaker Toolkit",
 	"description": "Documentation and resources to ease the process of creating custom cards for the game Artificial Academy 2.",
-	"url": "https://aa2ct.github.io",
+	"url": "https://r00taryengine.github.io/AA2CardmakerToolkit",
 	"env": process.env.ELEVENTY_ENV === 'production'
 }
