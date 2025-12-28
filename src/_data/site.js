f9e5d2c098de06d322b43b9c7e2a5c62fbfcffee
R00taryEnginE <13239555+R00taryEnginE@users.noreply.github.com>
2025-12-28 14:52:30 +0200

    Added footer, edit button
    Signed-off-by: R00taryEnginE <13239555+R00taryEnginE@users.noreply.github.com>

--- src/_data/site.js
+++ src/_data/site.js
@@ -2,5 +2,7 @@ module.exports = {
 	"title": "AA2 Cardmaker Toolkit",
 	"description": "Documentation and resources to ease the process of creating custom cards for the game Artificial Academy 2.",
 	"url": "https://r00taryengine.github.io/AA2CardmakerToolkit",
+	"repo_url": "https://github.com/R00taryEnginE/AA2CardmakerToolkit",
+	"repo_branch": "main",
 	"env": process.env.ELEVENTY_ENV === 'production'
 }
